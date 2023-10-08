5e272338bb33e1f27cefacbff58bcb7a21143e14
tachtachtach <thutrang123888@gmail.com>
2023-10-08 12:38:48 +0700

    Modify Class Account

--- src/main/Account.java
+++ src/main/Account.java
@@ -1,9 +1,13 @@
 package main;
 
+import java.util.Scanner;
+
 public class Account {
     private long accNumber;
     private String accName;
     private double balanceMoney;
+    private final double RATE = 0.035;
+
 
     public Account() {
         System.out.println("No param");
@@ -13,7 +17,18 @@ public class Account {
         System.out.println("All param");
         this.accNumber = accNumber;
         this.accName = accName;
+        if (balanceMoney < 0) {
+            return;
+        }
         this.balanceMoney = balanceMoney;
+
+    }
+
+    public Account(long accNumber, String accName) {
+        System.out.println("Ten TK va STK");
+        this.accNumber = accNumber;
+        this.accName = accName;
+        this.balanceMoney = 50;
     }
 
     public long getAccNumber() {
@@ -38,12 +53,36 @@ public class Account {
     }
 
     public void setBalanceMoney(double balanceMoney) {
+        while (balanceMoney < 0) {
+            System.out.println("should enter the positive");
+            balanceMoney = new Scanner(System.in).nextDouble();
+        }
         this.balanceMoney = balanceMoney;
+
+
     }
 
     @Override
     public String toString() {
-        return "Stk cua " + this.accName + "la: " + this.accNumber + "con: " + this.balanceMoney;
+        return "The number account of " + this.accName + "is: " + this.accNumber + "with money =: " + this.balanceMoney;
+    }
+
+    //    Nạp tiền
+    public void addMoney(double addMoney) {
+        this.balanceMoney = this.balanceMoney + addMoney;
+        System.out.println("New balance: " + this.balanceMoney);
+    }
+
+    //    Rút tiền
+    public void withdraw(double withdraw) {
+        this.balanceMoney = this.balanceMoney - withdraw;
+        System.out.println("New balance: " + this.balanceMoney);
+    }
+
+    //    Đáo hạn
+    public void expire() {
+        this.balanceMoney = this.balanceMoney + this.balanceMoney * this.RATE;
+        System.out.println("New balance: " + this.balanceMoney);
     }
 
 }
